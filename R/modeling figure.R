87a6d7b513935455c9f7e38f5ed66563fcf43a6d
John Drake <je.drake@westernsydney.edu.au>
2017-03-15 13:16:29 +1100

    Updated parameters, added model runs using BB and BBLeuning

--- R/modeling figure.R
+++ R/modeling figure.R
@@ -35,11 +35,23 @@ dat <- subset(dat.hr,DateTime_hr>starttime & DateTime_hr<endtime)
 # Look at the model predictions for the ambient treatment.  
 
 test <- subset(dat,HWtrt=="C" & PAR >10)
-g0 <- 0.001
+
+
+#- fit g1 and g0 for the "test" dataset
+gs_fits <- nls(gs ~ g0+1.6*(1+g1/sqrt(VPD))*(Photo/400),start=list(g0=0.002,g1=4),data=subset(test,PAR>500),algorithm="port",
+                      lower=c(0,0),upper=c(0.005,10))
+
+g0 = unname(coef(gs_fits)[1])
+g1 = unname(coef(gs_fits)[2])
+g1 = 2.9
+#g0 <- 0.001
+#g1 <- 3
+#g0=0.02
+#g1=2.5 
 pred1 <- suppressMessages(PhotosynEB(Tair=test$Tair_al,VPD=test$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
                                      LeafAbs=0.86,
-                                     PPFD=test$PAR,g1=3,g0=g0,
-                                     Vcmax=35,EaV=51780,EdVC=2e5,delsC=645,
+                                     PPFD=test$PAR,g1=g1,g0=g0,
+                                     Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
                                      Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
 table(pred1$failed)
 pred1$Tdiff <- with(pred1,Tleaf-Tair)
@@ -82,8 +94,8 @@ plot(pred1$Tleaf~test$TargTempC_Avg);abline(0,1,col="red")
 test.hw <- subset(dat,HWtrt=="HW" & PAR >10)
 pred2 <- suppressMessages(PhotosynEB(Tair=test.hw$Tair_al,VPD=test.hw$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
                                      LeafAbs=0.86,
-                                     PPFD=test.hw$PAR,g1=3,g0=g0,
-                                     Vcmax=35,EaV=51780,EdVC=2e5,delsC=645,
+                                     PPFD=test.hw$PAR,g1=g1,g0=g0,
+                                     Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
                                      Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
 table(pred2$failed)
 pred2$Tdiff <- with(pred2,Tleaf-Tair)
@@ -226,7 +238,8 @@ legend("topright",c("Model","Control data","Heatwave data"),pch=c(3,16,16),col=c
 legend("topleft",letters[1],bty="n")
 
 dc2 <- densCols(test.hwhigh$Photo,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
-plot(test.hwhigh$Photo~test.hwhigh$TargTempC_Avg,col=dc2,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n")
+plot(test.hwhigh$Photo~test.hwhigh$TargTempC_Avg,col=dc2,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n",
+     ylab="",xlab="")
 points(ALEAF~Tleaf,data=subset(pred2,PPFD>PARlimit),pch=3,cex=ptsize)
 legend("topleft",letters[2],bty="n")
 axis(1,tck=0.025,labels=F);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
@@ -247,9 +260,33 @@ legend("topleft",letters[4],bty="n")
 axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
 
 
-title(xlab=expression(Leaf~temperature~(T[leaf]~degree*C)),outer=T,cex.lab=2,line=4)
+title(xlab=expression(Canopy~temperature~(degree*C)),outer=T,cex.lab=2,line=4)
 title(ylab=expression(A[canopy]~(mu*mol~CO[2]~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.9)
 title(ylab=expression(E[canopy]~(mmol~H[2]*O~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.1)
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#- plot individual panels for presentation
+windows(20,20)
+ptsize=0.75
+par(mar=c(6,6,1,1),cex.lab=2,las=1,cex.axis=1.1)
+dc2 <- densCols(test.hwhigh$Photo,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Photo~test.hwhigh$TargTempC_Avg,col=dc2,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n",
+     ylab=expression(Photosynthesis~(mu*mol~m^-2~s^-1)),xlab=expression(T[leaf]~(degree*C)))
+points(ALEAF~Tleaf,data=subset(pred2,PPFD>PARlimit),pch=3,cex=ptsize)
+legend("topright",c("Model","Heatwave data"),pch=c(3,16),col=c("black","red"),cex=1.4,bg="white")
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
+
+dc4 <- densCols(test.hwhigh$Trans,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Trans~test.hwhigh$TargTempC_Avg,col=dc4,pch=16,cex=1,ylim=c(0,3.5),xlim=c(15,45),xaxt="n",yaxt="n",
+     ylab=expression(Transpiration~(mmol~m^-2~s^-1)),xlab=expression(T[leaf]~(degree*C)))
+points(ELEAF~Tleaf,data=subset(pred2,PPFD>PARlimit & failed==F),pch=3,cex=ptsize)
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
 
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
@@ -269,5 +306,178 @@ summary(lm.E)$r.squared #r2
 lm.leafT <- lm(test$TargTempC_Avg~pred1$Tleaf)
 summary(lm.leafT)$sigma #RMSE
 summary(lm.leafT)$r.squared #r2
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#- repeat for ball-berry
+
+pred1BB <- suppressMessages(PhotosynEB(Tair=test$Tair_al,VPD=test$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
+                                     LeafAbs=0.86,
+                                     PPFD=test$PAR,
+                                     gsmodel="BallBerry",g1=9,g0=0.01,
+                                     Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
+                                     Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
+
+pred2BB <- suppressMessages(PhotosynEB(Tair=test.hw$Tair_al,VPD=test.hw$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
+                                     LeafAbs=0.86,
+                                     PPFD=test.hw$PAR,g1=9,g0=0.01,
+                                     gsmodel="BallBerry",
+                                     Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
+                                     Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
+
+
+windows(80,70)
+par(mar=c(0,0,0,0),oma=c(6,6,3,5),cex.lab=1.6,las=1,cex.axis=1.2)
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE), 
+       widths=c(2,2), heights=c(2,2))
+ptsize=0.3
+PARlimit=600
+
+#- extract the control data above the PARlimit
+testhigh <- subset(test,PAR>PARlimit)
+test.hwhigh <- subset(test.hw,PAR>PARlimit)
+
+#palette(c("#00A2FF", "#F5690C", "#1C41D6", "#FF0A0A")) #shades of blue and red
+
+#- install the geneplotter package to make density plots
+## try http:// if https:// URLs are not supported
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("geneplotter")
+library(geneplotter); library(RColorBrewer)
+blues.ramp <- colorRampPalette(brewer.pal(9,"Blues")[-1],alpha=0.7)
+reds.ramp <- colorRampPalette(brewer.pal(9,"Reds")[-1],alpha=0.7)
+
+
+#- plot Photosynthesis vs. Tleaf. 
+dc <- densCols(testhigh$Photo,testhigh$TargTempC_Avg,colramp=blues.ramp)
+plot(testhigh$Photo~testhigh$TargTempC_Avg,col=dc,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ALEAF~Tleaf,data=subset(pred1BB,PPFD>PARlimit),pch=3,cex=ptsize,ylim=c(0,12),xlim=c(15,45))
+axis(1,tck=0.025,labels=F);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
+legend("topright",c("Model","Control data","Heatwave data"),pch=c(3,16,16),col=c("black",dc[1],"red"),cex=1.2,bg="white")
+legend("topleft",letters[1],bty="n")
+
+dc2 <- densCols(test.hwhigh$Photo,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Photo~test.hwhigh$TargTempC_Avg,col=dc2,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n",
+     ylab="",xlab="")
+points(ALEAF~Tleaf,data=subset(pred2BB,PPFD>PARlimit),pch=3,cex=ptsize)
+legend("topleft",letters[2],bty="n")
+axis(1,tck=0.025,labels=F);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
+
+
+#- transiration
+dc3 <- densCols(testhigh$Trans,testhigh$TargTempC_Avg,colramp=blues.ramp)
+plot(testhigh$Trans~testhigh$TargTempC_Avg,col=dc3,pch=16,cex=1,ylim=c(0,4),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ELEAF~Tleaf,data=subset(pred1BB,PPFD>PARlimit & failed==F),pch=3,cex=ptsize)
+legend("topleft",letters[3],bty="n")
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
+
+
+dc4 <- densCols(test.hwhigh$Trans,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Trans~test.hwhigh$TargTempC_Avg,col=dc4,pch=16,cex=1,ylim=c(0,4),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ELEAF~Tleaf,data=subset(pred2BB,PPFD>PARlimit & failed==F),pch=3,cex=ptsize)
+legend("topleft",letters[4],bty="n")
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
+
+
+title(xlab=expression(Canopy~temperature~(degree*C)),outer=T,cex.lab=2,line=4)
+title(ylab=expression(A[canopy]~(mu*mol~CO[2]~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.9)
+title(ylab=expression(E[canopy]~(mmol~H[2]*O~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.1)
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+
+
+
+
+
+
+
+#---------------------------------------------------------------------------
+#---------------------------------------------------------------------------
+#- repeat for Leuning
+
+pred1Leuning<- suppressMessages(PhotosynEB(Tair=test$Tair_al,VPD=test$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
+                                       LeafAbs=0.86,
+                                       PPFD=test$PAR,
+                                       gsmodel="BBLeuning",g1=7,g0=0.01,
+                                       Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
+                                       Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
+
+pred2Leuning <- suppressMessages(PhotosynEB(Tair=test.hw$Tair_al,VPD=test.hw$VPD,Wind=8,Wleaf=0.01,StomatalRatio=1,
+                                       LeafAbs=0.86,
+                                       PPFD=test.hw$PAR,g1=7,g0=0.01,
+                                       gsmodel="BBLeuning",
+                                       Vcmax=34,EaV=51780,EdVC=2e5,delsC=640,
+                                       Jmax = 60,EaJ=21640,EdVJ=2e5,delsJ=633))
+
+
+windows(80,70)
+par(mar=c(0,0,0,0),oma=c(6,6,3,5),cex.lab=1.6,las=1,cex.axis=1.2)
+layout(matrix(c(1,2,3,4), 2, 2, byrow = TRUE), 
+       widths=c(2,2), heights=c(2,2))
+ptsize=0.3
+PARlimit=600
+
+#- extract the control data above the PARlimit
+testhigh <- subset(test,PAR>PARlimit)
+test.hwhigh <- subset(test.hw,PAR>PARlimit)
+
+#palette(c("#00A2FF", "#F5690C", "#1C41D6", "#FF0A0A")) #shades of blue and red
+
+#- install the geneplotter package to make density plots
+## try http:// if https:// URLs are not supported
+#source("https://bioconductor.org/biocLite.R")
+#biocLite("geneplotter")
+library(geneplotter); library(RColorBrewer)
+blues.ramp <- colorRampPalette(brewer.pal(9,"Blues")[-1],alpha=0.7)
+reds.ramp <- colorRampPalette(brewer.pal(9,"Reds")[-1],alpha=0.7)
+
+
+#- plot Photosynthesis vs. Tleaf. 
+dc <- densCols(testhigh$Photo,testhigh$TargTempC_Avg,colramp=blues.ramp)
+plot(testhigh$Photo~testhigh$TargTempC_Avg,col=dc,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ALEAF~Tleaf,data=subset(pred1Leuning,PPFD>PARlimit),pch=3,cex=ptsize,ylim=c(0,12),xlim=c(15,45))
+axis(1,tck=0.025,labels=F);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
+legend("topright",c("Model","Control data","Heatwave data"),pch=c(3,16,16),col=c("black",dc[1],"red"),cex=1.2,bg="white")
+legend("topleft",letters[1],bty="n")
+
+dc2 <- densCols(test.hwhigh$Photo,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Photo~test.hwhigh$TargTempC_Avg,col=dc2,pch=16,cex=1,ylim=c(0,12),xlim=c(15,45),xaxt="n",yaxt="n",
+     ylab="",xlab="")
+points(ALEAF~Tleaf,data=subset(pred2Leuning,PPFD>PARlimit),pch=3,cex=ptsize)
+legend("topleft",letters[2],bty="n")
+axis(1,tck=0.025,labels=F);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
+
+
+#- transiration
+dc3 <- densCols(testhigh$Trans,testhigh$TargTempC_Avg,colramp=blues.ramp)
+plot(testhigh$Trans~testhigh$TargTempC_Avg,col=dc3,pch=16,cex=1,ylim=c(0,4),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ELEAF~Tleaf,data=subset(pred1Leuning,PPFD>PARlimit & failed==F),pch=3,cex=ptsize)
+legend("topleft",letters[3],bty="n")
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=T);axis(4,tck=0.025,labels=F)
+
+
+dc4 <- densCols(test.hwhigh$Trans,test.hwhigh$TargTempC_Avg,colramp=reds.ramp)
+plot(test.hwhigh$Trans~test.hwhigh$TargTempC_Avg,col=dc4,pch=16,cex=1,ylim=c(0,4),xlim=c(15,45),xaxt="n",yaxt="n")
+points(ELEAF~Tleaf,data=subset(pred2Leuning,PPFD>PARlimit & failed==F),pch=3,cex=ptsize)
+legend("topleft",letters[4],bty="n")
+axis(1,tck=0.025,labels=T);axis(2,tck=0.025,labels=F);axis(4,tck=0.025,labels=T)
+
+
+title(xlab=expression(Canopy~temperature~(degree*C)),outer=T,cex.lab=2,line=4)
+title(ylab=expression(A[canopy]~(mu*mol~CO[2]~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.9)
+title(ylab=expression(E[canopy]~(mmol~H[2]*O~m^-2~s^-1)),outer=T,cex.lab=1.5,line=2.5,adj=0.1)
+
 #---------------------------------------------------------------------------
 #---------------------------------------------------------------------------
